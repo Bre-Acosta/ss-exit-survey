2ed77e733d7a277331ad2c268c2cc296f58a9148
Bre-Acosta <bre.laci.acosta@gmail.com>
2025-08-08 18:32:30 -0500

    Fix URL validation logic in form handler
    - Fixed bug where URL validation was comparing against the actual URL instead of placeholder
    - Now properly validates that URL is configured and not the default placeholder
    - Form should now submit to Google Apps Script correctly

--- form-handler.js
+++ form-handler.js
@@ -4,7 +4,7 @@
 class SurveyFormHandler {
     constructor() {
         // Replace with your deployed Google Apps Script web app URL
-        this.scriptURL = 'https://script.google.com/macros/s/AKfycbz5Aat3DClIIMvQraOnhdKVGmAV2AuhgXF1Xudu8Va096nL87nlbh8P9p4ACAF_yhPPcQ/exec';
+        this.scriptURL = 'https://script.google.com/macros/s/AKfycbwwv_GWgi9Joy5DcjJbzi4NvoQ-sbhwISzFu01Yy3PjvNxQ59cThIbv9k6_MPibdsUSpw/exec';
         this.form = document.getElementById('exitSurvey');
         this.submitBtn = document.querySelector('.submit-btn');
         this.init();
